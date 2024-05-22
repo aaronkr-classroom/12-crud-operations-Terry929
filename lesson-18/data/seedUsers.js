18ebff8c0bedbf1cf7de5b378c874947d50e7815
이준혁 <103807152+Terry929@users.noreply.github.com>
2024-05-22 02:33:45 +0000

    끝!

--- lesson-18/data/seedUsers.js
+++ lesson-18/data/seedUsers.js
@@ -8,10 +8,14 @@ const mongoose = require("mongoose"),
   User = require("../models/User");
 
 // 데이터베이스 연결 설정
-mongoose.connect("mongodb://127.0.0.1:27017/ut-nodejs", {
-  useNewUrlParser: true,
-});
-
+mongoose.connect(
+    "mongodb+srv://ut-node:GrXEpqODFPP75bRL@ut-node.tceekpm.mongodb.net/?retryWrites=true&w=majority&appName=UT-Node", //Atlas 경로 
+  );
+const db = mongoose.connection;
+db.once("open",() => {
+  console.log("Connected to DB!");
+  });
+  
 mongoose.connection;
 
 /** 
@@ -157,4 +161,4 @@ setTimeout(() => {
     .catch((error) => {
       console.log(`Error: ${error}`);
     });
-}, 500);
+}, 1500);
